e086f612489bcc41dc840d19d8e06d8d7e44400f
gustavoaluiz <gustavoaluizbp@gmail.com>
2025-10-31 14:47:51 -0300

    chore: update package installation in prepare_ambient.sh to use specific versions and improve dependency management

--- prepare_ambient.sh
+++ prepare_ambient.sh
@@ -10,11 +10,11 @@ conda install cuda -c nvidia
 
 # Instalações de Python (na ordem do Dockerfile)
 $PIP install azureml-mlflow tensorboard
-$PIP install packaging lightning
-$PIP install 'jsonargparse[signatures]' tokenizers sentencepiece wandb torchmetrics
+$PIP install -U packaging setuptools wheel ninja cmake
+$PIP install 'jsonargparse[signatures]' tokenizers sentencepiece wandb torchmetrics lightning
 $PIP install tensorboard zstandard pandas pyarrow huggingface_hub
 $PIP -U --no-build-isolation --no-cache-dir \
-  'https://github.com/Dao-AILab/flash-attention/releases/download/v2.8.3/flash_attn-2.8.3+cu12torch2.8cxx11abiTRUE-cp312-cp312-linux_x86_64.whl'
+  'https://github.com/Dao-AILab/flash-attention/releases/download/v2.5.8/flash_attn-2.5.8+cu122torch2.3cxx11abiTRUE-cp310-cp310-linux_x86_64.whl'
 
 # flash-attention (componentes específicos)
 if [ ! -d flash-attention ]; then
@@ -38,8 +38,8 @@ cd /app
 
 # Demais libs
 $PIP install transformers==4.46.1 numpy accelerate
-$PIP install causal-conv1d --no-build-isolation
-$PIP install mamba-ssm --no-build-isolation
+$PIP install --no-cache-dir "https://github.com/Dao-AILab/causal-conv1d/releases/download/v1.4.0/causal_conv1d-1.4.0+cu122torch2.4cxx11abiTRUE-cp310-cp310-linux_x86_64.whl"
+$PIP install --no-cache-dir "https://github.com/state-spaces/mamba/releases/download/v2.2.6.post3/mamba_ssm-2.2.6.post3+cu12torch2.4cxx11abiTRUE-cp310-cp310-linux_x86_64.whl"
 # $PIP install torchao --extra-index-url https://download.pytorch.org/whl/cu128
 # $PIP install triton==3.2.0
 # $PIP install einops
